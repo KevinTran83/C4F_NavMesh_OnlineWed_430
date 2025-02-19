bc7fb3ffc6462003e508a2dfaa59bf2edd4859b6
Kevin S. Tran <tanksense@gmail.com>
2025-02-19 17:30:18 +1100

    Week 3

--- Assets/NavMeshing/Scripts/Boom.cs
+++ Assets/NavMeshing/Scripts/Boom.cs
@@ -4,15 +4,18 @@ public class Boom : MonoBehaviour
 {
     public GameObject target;
 
+    private Vector3 localPos;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        localPos = target.transform
+            .InverseTransformPoint(transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.transform.position;
+        transform.position = target.transform.position + localPos;
     }
 }
